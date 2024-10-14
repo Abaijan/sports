2b4be25cec778672559656361cdf46370812357e
Abaijan <fghf09647@gmail.com>
2024-10-14 22:59:46 +0600

    Initial commit from Create Next App

---
+++ app/page.js
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+      .main
+      .content
+      .container
+
+      .row
+      .row-cols-1
+      .row-cols-md-2
+      .g-4
+      .justify-content-md-center
+      .align-items-md-center
+      .text-center
+  );
+}
