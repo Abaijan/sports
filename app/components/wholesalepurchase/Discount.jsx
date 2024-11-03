cfc55b31ef0053f34cfdc3ef1220bf03f1e24908
ArgenTemiraliev <temiraliev0304@gmail.com>
2024-11-03 14:28:20 +0600

    wholesale

--- app/components/wholesalepurchase/Discount.jsx
+++ app/components/wholesalepurchase/Discount.jsx
@@ -4,9 +4,9 @@ export  function Discount() {
   return (
     <section>
         <section className=' container'>
-            <section className=' flex flex-col justify-center text-center p-10'>
-                <h2 className=' text-blue-700 text-5xl mx-96'>При покупке от 100 штук вы получаете скидку 10%</h2>
-                <p className=' text-sm mx-72 p-3' >Мы — команда, которая любит спорт и знает, что нужно для эффективных тренировок.
+            <section className=' flex flex-col justify-center items-center text-center p-5 '>
+                <h2 className=' text-blue-700 lg:text-4xl  mx-36 md:text-xl '>При покупке от 100 штук вы получаете скидку 10%</h2>
+                <p className='  md:mx-48 text-sm ' >Мы — команда, которая любит спорт и знает, что нужно для эффективных тренировок.
                      В нашем магазине представлены лучшие спортивные товары и экипировка, которые подходят как для новичков,
                       так и для профессионалов. Мы предлагаем качественные решения для активной жизни и поддерживаем каждого на пути к здоровью и успеху.</p>
             </section>
